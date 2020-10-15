cf900937678eaf777c30a8048927246b7308b4ca
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-15 09:41:30 -0400

    Initial Commit

---
+++ MathPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MathPractice
+//
+//  Created by Cloutier, Vincent on 2020-10-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MathPractice/MathPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathPracticeApp.swift
+//  MathPractice
+//
+//  Created by Cloutier, Vincent on 2020-10-15.
+//
+
+import SwiftUI
+
+@main
+struct MathPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
