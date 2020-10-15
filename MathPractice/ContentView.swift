b970f3c803c6fbb7b598f7eb1d64e6c152bcb5cc
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-15 09:48:33 -0400

    building a test math practice ui to test logic, once logic is done will make very nice

--- MathPractice/ContentView.swift
+++ MathPractice/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Text("+")
+                Text("-")
+                Text("*")
+                Text("/")
+            }
+            .navigationBarTitle(Text("Math Practice"))
+        }
     }
 }
 
