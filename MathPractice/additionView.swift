370b484806da9f3ffcb50569c16da1569f2da414
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-15 10:50:54 -0400

    testing out logic somethings wrong

--- MathPractice/additionView.swift
+++ MathPractice/additionView.swift
@@ -10,28 +10,24 @@ import SwiftUI
 struct additionView: View {
 @State private var input = ""
 @State private var score = 1
-var number1 = Int.random(in: 1..<12)
-var number2 = Int.random(in: 1..<12)
-    //var answer = (number1 + number2)
+let operations = ["+","-","*","/"]
+let number1 = [1,2,3,4,5,6,7,8,9,10,11,12]
+let number2 = [1,2,3,4,5,6,7,8,9,10,11,12]
+let equation = "\(number1.shuffled())\(operations.shuffled()) \(number2.shuffled())"
+
+let answer = 0
+
     
     var body: some View {
         VStack{
         Text("Score:\(score)")
-        Text("\(number1) + \(number2)")
+        Text(equation)
         TextField("Input", text: $input).keyboardType(.decimalPad)
         //button this matters
         }
     }
 }
-func answerCheck() {
-    if input == answer {
-        score += 1
-        //refreshnumbers
-    }
-    else {
-        //popup displaying error message
-    }
-}
+
 struct additionView_Previews: PreviewProvider {
     static var previews: some View {
         additionView()
