2229014f9d37833f04d2f4cc3219cdb1517aad6e
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-16 11:50:34 -0400

    Logic is finally solid for real. except division

--- MathPractice/ContentView.swift
+++ MathPractice/ContentView.swift
@@ -8,15 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var input = ""
+    @State private var score = 0
+    @State private var number1 = Int.random(in: 0..<12)
+    @State private var number2 = Int.random(in: 0..<12)
+    @State private var operatorNumber = Int.random(in: 0..<4)
+    @State private var answer = 0
     var body: some View {
         NavigationView {
-            List {
-                NavigationLink("+", destination: additionView())
-                NavigationLink("+", destination: additionView())
-                NavigationLink("+", destination: additionView())
-                NavigationLink("+", destination: additionView())
+            VStack{
+                // Display score
+                Text("Score: \(score)")
+                // Handle the randomized equation and set the answer
+                if operatorNumber == 0 {
+                    Text("\(number1) + \(number2)")
+                }
+                else if operatorNumber == 1 {
+                    Text("\(number1) - \(number2)")
+                }
+                else if operatorNumber == 2 {
+                    Text("\(number1) * \(number2)")
+                }
+                else if operatorNumber == 3 {
+                    Text("\(number1) / \(number2)")
+                }
+                // Input for user answer
+                TextField("Input", text: $input).keyboardType(.decimalPad)
+                Button(action: {checkAnswer()}, label: { Text("Click Me") })
             }
-            .navigationBarTitle(Text("Math Practice"))
+        }
+        .navigationBarTitle(Text("Math Practice"))
+    }
+    func checkAnswer() {
+        // Set answer
+        switch operatorNumber {
+        case 0:
+            answer = number1 + number2
+        case 1:
+            answer = number1 - number2
+        case 2:
+            answer = number1 * number2
+        case 3:
+            answer = number1 / number2
+        default:
+            break
+        }
+        // Turn string into Int
+        let intInput = Int(input) ?? 0
+        if answer == intInput {
+            // Reward users for right answers
+            score += 1
+            // Make a new equation
+            number1 = Int.random(in: 0..<12)
+            number2 = Int.random(in: 0..<12)
+            operatorNumber = Int.random(in: 0..<4)
+        }
+        else if score > 0 && answer != intInput{
+            // Punish users for wrong answers
+            score -= 1
+            // Make a new equation
+            number1 = Int.random(in: 0..<12)
+            number2 = Int.random(in: 0..<12)
+            operatorNumber = Int.random(in: 0..<4)
         }
     }
 }
--- MathPractice/additionView.swift
+++ MathPractice/additionView.swift
@@ -10,37 +10,64 @@ import SwiftUI
 struct additionView: View {
     @State private var input = ""
     @State private var score = 0
-    let number1 = Int.random(in: 0..<12)
-    let number2 = Int.random(in: 0..<12)
-    let question = Int.random(in: 0..<4)
-    var answer = 0
+    @State private var number1 = Int.random(in: 0..<12)
+    @State private var number2 = Int.random(in: 0..<12)
+    @State private var operatorNumber = Int.random(in: 0..<4)
+    @State private var answer = 0
     var body: some View {
         
         VStack{
+            // Display score
             Text("Score: \(score)")
-            switch question {
-            case 0: Text("\(number1) + \(number2)")
-                 var answer = number1 + number2
-            case 1: Text("\(number1) - \(number2)")
-                var answer = number1 - number2
-            case 2: Text("\(number1) / \(number2)")
-                var answer = number1 / number2
-            case 3: Text("\(number1) * \(number2)")
-                var answer = number1 * number2
-            default: Text("ERROR")
+            // Handle the randomized equation and set the answer
+            if operatorNumber == 0 {
+                Text("\(number1) + \(number2)")
             }
+            else if operatorNumber == 1 {
+                Text("\(number1) - \(number2)")
+            }
+            else if operatorNumber == 2 {
+                Text("\(number1) * \(number2)")
+            }
+            else if operatorNumber == 3 {
+                Text("\(number1) / \(number2)")
+            }
+            // Input for user answer
             TextField("Input", text: $input).keyboardType(.decimalPad)
             Button(action: {checkAnswer()}, label: { Text("Click Me") })
         }
     }
     func checkAnswer() {
+        // Set answer
+        switch operatorNumber {
+        case 0:
+            answer = number1 + number2
+        case 1:
+            answer = number1 - number2
+        case 2:
+            answer = number1 * number2
+        case 3:
+            answer = number1 / number2
+        default:
+            break
+        }
+        // Turn string into Int users
         let intInput = Int(input) ?? 0
         if answer == intInput {
+            // Reward users for right answers
             score += 1
-            //new equation
+            // Make a new equation
+            number1 = Int.random(in: 0..<12)
+            number2 = Int.random(in: 0..<12)
+            operatorNumber = Int.random(in: 0..<4)
         }
-        else {
-            print("error")
+        else if score > 0 && answer != intInput{
+            // Punish users for wrong answers
+            score -= 1
+            // Make a new equation
+            number1 = Int.random(in: 0..<12)
+            number2 = Int.random(in: 0..<12)
+            operatorNumber = Int.random(in: 0..<4)
         }
     }
 }
