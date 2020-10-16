0cfca0c4830b37d55f3f4f133e0093971425da09
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-16 11:20:49 -0400

    logic looks sound I believe whats stopping me is a syntax error

--- MathPractice/additionView.swift
+++ MathPractice/additionView.swift
@@ -8,25 +8,39 @@
 import SwiftUI
 
 struct additionView: View {
-@State private var input = ""
-@State private var score = 1
-let number1 = Int.random(in: 0..<12)
-let number2 = Int.random(in: 0..<12)
-switch Int.random(in: 0..<4) {
-            case 0: question = "\(number1) + \(number2)" //set answer too
-            case 1: question = "\(number1) - \(number2)"
-            case 2: question = "\(number1) / \(number2)"
-            case 3: question = "\(number1) * \(number2)"
-            default: question = "ERROR"
-        }
-    
+    @State private var input = ""
+    @State private var score = 0
+    let number1 = Int.random(in: 0..<12)
+    let number2 = Int.random(in: 0..<12)
+    let question = Int.random(in: 0..<4)
+    var answer = 0
     var body: some View {
         
         VStack{
-        Text("Score:\(score)")
-            Text(question)
-        TextField("Input", text: $input).keyboardType(.decimalPad)
-        //button this matters
+            Text("Score: \(score)")
+            switch question {
+            case 0: Text("\(number1) + \(number2)")
+                 var answer = number1 + number2
+            case 1: Text("\(number1) - \(number2)")
+                var answer = number1 - number2
+            case 2: Text("\(number1) / \(number2)")
+                var answer = number1 / number2
+            case 3: Text("\(number1) * \(number2)")
+                var answer = number1 * number2
+            default: Text("ERROR")
+            }
+            TextField("Input", text: $input).keyboardType(.decimalPad)
+            Button(action: {checkAnswer()}, label: { Text("Click Me") })
+        }
+    }
+    func checkAnswer() {
+        let intInput = Int(input) ?? 0
+        if answer == intInput {
+            score += 1
+            //new equation
+        }
+        else {
+            print("error")
         }
     }
 }
