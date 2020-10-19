d0c604580ebbc700e10b98a23ec9138438287134
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-19 15:07:47 -0400

    error with compilation, code looks fine, animations imported

--- MathPractice/LottieView.swift
+++ MathPractice/LottieView.swift
@@ -10,4 +10,34 @@ import Lottie
 
 struct LottieView: UIViewRepresentable {
     @State var animationNamed:String
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        let view = UIView(frame: .zero)
+        //Lottie View
+        let animationView = AnimationView()
+        let animation = Animation.named(animationNamed)
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.play()
+        //END -> Lottie View
+        //Constraints
+        animationView.translatesAutoresizingMaskIntoConstraints =
+            false
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo:view.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo:view.heightAnchor)
+        ])
+        //END -> Constraints
+        return view
+    }
+    func updateUiView(_ uiView: UIView, context: Context){
+        
+    }
+    typealias UIViewType = UIView
+}
+struct LottieView_Previews: PreviewProvider {
+    static var previews: some View {
+        LottieView(animationNamed: "correct")
+    }
 }
