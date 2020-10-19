664c59c4e7090d9f0fd7f91d40908db673522d94
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-19 14:53:38 -0400

    starting lottieView

--- MathPractice/LottieView.swift
+++ MathPractice/LottieView.swift
@@ -5,7 +5,9 @@
 //  Created by Cloutier, Vincent on 2020-10-19.
 //
 
-import Foundation
-struct <#name#> {
-    <#fields#>
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    @State var animationNamed:String
 }
